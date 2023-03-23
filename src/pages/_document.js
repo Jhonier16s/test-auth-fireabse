a9ccb0edfbd032b477861b101b3210978a703337
Jhonier <jhonier.becerra@zoomcem.com>
2023-03-23 09:56:26 -0500

    fiexed document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <link rel="manifest" href="/manifest.json" />
-      <link rel="apple-touch-icon" href="/icon-192x192.png" />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
